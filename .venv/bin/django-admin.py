ec98167614891b665a4bf8a607e27842cbcf62a4
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-09-27 01:52:03 -0700

    testing esql deployment

---
+++ .venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/vijayetar/codefellows/401/projects/esql-groceries/.venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
